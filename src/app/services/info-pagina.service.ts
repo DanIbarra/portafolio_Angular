0668666c014724d8371243c337bf878d98184ebe
Daniel <fcodany123@gmail.com>
2021-06-21 01:54:07 -0700

    Intento no. 2 de hacer funcionar el proyecto

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+  constructor( private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+    //Leer el archivo JSON
+
+    
+
+   };
+
+   private cargarInfo(){
+
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina )=> {
+
+        this.cargada=true;
+        this.info= resp;
+
+      });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-93f3c-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[] )=> {
+
+        
+        this.equipo= resp;
+
+   
+
+      });
+   }
+}
